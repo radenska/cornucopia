320ae026b7a1ad755f109ea43cf10c92c9e0cadc
radenska <yana_radenska@yahoo.com>
2017-04-13 13:09:21 -0700

    added navbar component

---
+++ app/component/navbar/navbar.js
@@ -0,0 +1,33 @@
+'use strict';
+
+require('./_navbar.scss');
+
+module.exports = {
+  template: require('./navbar.html'),
+  controller: ['$log', '$location', '$rootScope', 'authService', NavbarController],
+  controllerAs: 'navbarCtrl'
+};
+
+function NavbarController($log, $location, $rootScope, authService) {
+  $log.debug('NavbarController');
+
+  this.checkPath = function() {
+    $log.debug('NavbarController.checkPath');
+    let path = $location.path();
+    if (path === '/join') this.hideBtn = true;
+    if (path !== '/join') this.hideBtn = false;
+
+    authService.getToken()
+    .catch( () => $location.url('/join#login'));
+  }
+
+  this.checkPath();
+  $rootScope.$on('$locationChangeSuccess', () => this.checkPath());
+
+  this.logout = function() {
+    $log.debug('NavbarController.logout');
+    this.hideBtn = true;
+    authService.logout()
+    .then( () => $location.url('/'));
+  };
+}
