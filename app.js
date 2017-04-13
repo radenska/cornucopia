a64ea18df0b942e79400f5a185914f031f2fd4c2
radenska <yana_radenska@yahoo.com>
2017-04-13 11:09:33 -0700

    added chrome bits to app.js

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 {
   "environments": {
     "test": {
+      "scripts": {
+        "test": "npm run test"
+      },
+      "buildpacks": [
+        {
+          "url": "https://github.com/heroku/heroku-buildpack-google-chrome"
+        },
+        {
+          "url": "heroku/node"
+        }
+      ],
       "env": {
         "API_URL": "https://cornucopia-app.herokuapp.com"
+        "CHROME_BIN": ""
       }
     }
   }
