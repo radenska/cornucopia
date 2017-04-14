5139ea9e0b16fe853d38f2a6f32d95f7d9683eb8
0blu3 <braelynn.luedtke@gmail.com>
2017-04-14 13:30:05 -0700

    Profile view and edit-profile view mostly completed.

--- app/component/profile/profile-view/profile-view.js
+++ app/component/profile/profile-view/profile-view.js
@@ -3,13 +3,34 @@
 require('./_profile-view.scss');
 
 module.exports = {
-  template: require('./profile.html'),
+  template: require('./profile-view.html'),
   controller: ['$log', '$rootscope', ProfileViewController],
-  controllerAs: 'profileviewCtrl'
+  controllerAs: 'profileViewCtrl'
 };
 
-function ProfileViewController($log, $rootscope, profileService) {
+function ProfileViewController($log, $rootScope, profileService) {
   $log.debug('ProfileViewController');
 
   // add things here
-}
+
+  this.profiles = [];
+
+  this.fetchProfiles = function() {
+    profileService.fetchProfiles()
+    .then( profiles => {
+      this.profiles = profiles[0];
+    });
+  };
+
+  this.deleteProfile = function(profile) {
+    if (this.profile._id === profile._id) {
+      this.profile = null;
+    }
+  };
+
+  this.fetchProfiles();
+
+  $rootScope.$on('$locationChangeSuccess', () => {
+    this.fetchProfiles();
+  });
+};
